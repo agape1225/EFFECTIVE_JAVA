9cc72a171f506066e4dd34edbc0cc94e5a0ad712
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-01-07 22:26:45 +0900

    2024.01.07

--- Chapter 2/src/Main.java
+++ Chapter 2/src/Main.java
@@ -3,3 +3,50 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        //필수 매개변수
+        private final int servingSize;
+        private final int servings;
+
+        //선택 매개변수 - 기본값으로 초기화한다
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+    }
+
+    private NutritionFacts (Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
+
+NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohudrate(27).build();
