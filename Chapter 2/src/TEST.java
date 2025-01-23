f9b830cc86615dd5309ed8bac450ae14d971aea8
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-01-23 22:46:10 +0900

    2024.01.23

--- Chapter 2/src/TEST.java
+++ Chapter 2/src/TEST.java
@@ -1,6 +1,33 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TEST {
+    Map<PhoneNumber, String> m = new HashMap<>();
+    m.put(new PhoneNumber(707, 867, 5309), "JENNI" );
+
+    m.get(new PhoneNumber(707, 867, 5309));
+
+    @Override
+    public int hasCode() {
+        int result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+
+        return result;
+    }
+
+    /*
+    * 이 전화번호의 문자열 표현을 반환한다.
+    * 이 분자열은 "XXX-YYY-ZZZZ" 형태의 12글자로 구성된다.
+    * XXX는 지역 코드, YYY는 프리픽스, ZZZZ는 가입자 번호다.
+    * 각각의 대문자는 10진수 숫자 하나를 나타낸다.
+    * */
+    @Override
+    public String toString() {
+
+    }
+
     static String firstLineOfFile(String path, String defaultVal) throws IOException {
         try (
                 BufferedReader br = new BufferedReader(new FileReader(path));
